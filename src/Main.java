2e242f7a28459e77e1a40b45343e8a3f2ad381ac
kirsan07 <terkirsun@gmail.com>
2022-12-28 19:53:01 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+       базовый синтаксис :
+       while (condition) {
+       // тело цикла:
+       }
+         */
+        int i = 0;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
